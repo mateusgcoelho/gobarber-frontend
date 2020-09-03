de02696ec0133cb643f1aeebe1201cc292ab200d
João Mateus Games Coelho <mateusgamescoelho547@gmail.com>
2020-09-03 13:43:53 -0300

    Rotas autenticadas

---
+++ src/routes/Route.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import {
+  Route as ReactDOMRoute,
+  RouteProps as ReactRouteProps,
+  Redirect,
+} from 'react-router-dom';
+import { useAuth } from '../hooks/auth';
+
+interface RouteProps extends ReactRouteProps {
+  isPrivate?: boolean;
+  component: React.ComponentType;
+}
+
+// true/true = OK
+// true/false = Redirect Login
+// false/true = Redirect Dashboard
+// false/false = OK
+
+const Route: React.FC<RouteProps> = ({
+  isPrivate = false,
+  component: Component,
+  ...rest
+}) => {
+  const { user } = useAuth();
+
+  return (
+    <ReactDOMRoute
+      {...rest}
+      render={() => {
+        return isPrivate === !!user ? (
+          <Component />
+        ) : (
+          <Redirect to={{ pathname: isPrivate ? '/' : '/dashboard' }} />
+        );
+      }}
+    />
+  );
+};
+
+export default Route;

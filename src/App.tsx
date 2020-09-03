18c24960e88968a5e0243e1048c707c30d1f5d44
João Mateus Games Coelho <mateusgamescoelho547@gmail.com>
2020-09-03 11:36:51 -0300

    Estrutura de Toasts

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,13 @@ import SignIn from './pages/SignIn';
 import GlobalStyle from './styles/global';
 
 import { AuthProvider } from './hooks/AuthContext';
+import ToastContainer from './components/ToastContainer';
 
 const App: React.FC = () => {
   return (
     <>
       <AuthProvider>
+        <ToastContainer />
         <SignIn />
       </AuthProvider>
 

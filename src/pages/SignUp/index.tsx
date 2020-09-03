a6af5adc49e5d7ad1c73ab3f1ba0d440a4688a1d
João Mateus Games Coelho <mateusgamescoelho547@gmail.com>
2020-09-03 00:44:04 -0300

    Re upload aplicação com usabilidade alterada

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,50 @@
+/* eslint-disable @typescript-eslint/ban-types */
+import React from 'react';
+import { FiUser, FiLock, FiMail, FiArrowLeft } from 'react-icons/fi';
+import { Form } from '@unform/web';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Container, Content, Background } from './styles';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+const SignUp: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
+
+  return (
+    <Container>
+      <Background />
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
+
+        <Form initialData={{ name: 'Diego' }} onSubmit={handleSubmit}>
+          <h1>Faça seu cadastro</h1>
+
+          <Input name="name" icon={FiUser} placeholder="Nome" />
+
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
+
+          <Button type="submit">Cadastrar</Button>
+        </Form>
+
+        <a href="create">
+          <FiArrowLeft />
+          Voltar para logon
+        </a>
+      </Content>
+    </Container>
+  );
+};
+
+export default SignUp;

1561c69cad8531f26d7d5fcca661f038e7f80a5e
João Mateus Games Coelho <mateusgamescoelho547@gmail.com>
2020-10-04 21:36:16 -0300

    Pagina de perfil

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
 import { FiPower } from 'react-icons/fi';
 import { useAuth } from '../../hooks/auth';
 
@@ -25,7 +26,9 @@ const Header: React.FC = () => {
 
           <div>
             <span>Bem-vindo,</span>
-            <strong>{user.name}</strong>
+            <Link to="/profile">
+              <strong>{user.name}</strong>
+            </Link>
           </div>
         </Profile>
 
